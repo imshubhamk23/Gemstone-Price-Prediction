24c973c88068a0060b25d102f1db35b71f7eea91
Shubham Kolekar <115143382+imshubhamk23@users.noreply.github.com>
2023-04-28 09:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def predict_datapoint():
         return render_template('results.html',final_result=results)
         print(result)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=8080)
     
